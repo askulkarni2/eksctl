0a03259738539516ba18f0c9eff2def8d99b4079
Alfonso Acosta <fons@syntacticsugar.consulting>
2019-06-21 03:23:42 +0200

    Revert apis update
    Wait for it to be fixed separately (see
    https://github.com/weaveworks/eksctl/issues/918 )

--- pkg/apis/eksctl.io/v1alpha5/types.go
+++ pkg/apis/eksctl.io/v1alpha5/types.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/eks/eksiface"
 	"github.com/aws/aws-sdk-go/service/iam/iamiface"
 	"github.com/aws/aws-sdk-go/service/sts/stsiface"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -532,13 +533,7 @@ type (
 )
 
 // NodeGroupKubeletConfig contains extra config parameters for the kubelet.yaml
-type NodeGroupKubeletConfig map[string]Copyable
-
-// Copyable has the only purpose is to make the code generator happy
-// see https://github.com/kubernetes/code-generator/issues/50
-type Copyable interface {
-	DeepCopyCopyable() Copyable
-}
+type NodeGroupKubeletConfig map[string]interface{}
 
 // HasMixedInstances checks if a nodegroup has mixed instances option declared
 func HasMixedInstances(ng *NodeGroup) bool {
